d3996b72755752d6e10b4b3efea74b2ca1d2323d
Dani Barreto <94726541+Danibarretolima@users.noreply.github.com>
2022-03-07 15:36:42 -0300

    Finalizei o Ex016

--- Exercicios/Ex016 - Variaveis compostas/ambiente.js
+++ Exercicios/Ex016 - Variaveis compostas/ambiente.js
@@ -1,6 +1,23 @@
 let num = [5, 8, 2, 9, 3]
+
+
 num.push(1)
 num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posiçoes`)
+console.log(`O primeiro valor do vetor é ${num[1]}`)
+let pos = num.indexOf(4)
+if(pos == -1){
+    console.log('O valor não foi encontrado!')
+} else{
+    console.log(`O valor está na posição ${pos}`)
+}
+
+
+/*
+let num = [5, 8, 2, 9, 3]
+num.push(1) //vai colocar o valor escrito no () no ultimo vetor
+num.sort() //Vai colocar os valores de forma ordenada 
 console.log(num)
 console.log(`O vetor tem ${num.length} posiçoes`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
@@ -10,3 +27,4 @@ if (pos == -1){
 }  else{
     console.log(`O valor 8 esta a posição ${pos}`)
 }
+*/
