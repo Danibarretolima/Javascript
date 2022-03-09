caafa15d891359d3496293268c525a96e91c5d75
Dani Barreto <94726541+Danibarretolima@users.noreply.github.com>
2022-03-08 22:04:50 -0300

    Ex019

---
+++ Exercicios/Ex019 - Arrays/array.js
@@ -0,0 +1,34 @@
+//MANEIRA MAIS FACIL
+var aluno = ['Igor', 'José', "Marcos", 'Mariana','Joana', 'Leo', 'João','Tiago']
+
+for(var i in aluno){
+    console.log(aluno[i])
+}
+
+
+/*
+OUTRA MANEIRA
+var aluno = ['Igor', 'José', 'Marcos', 'Mariana','Joana', 'Leo', 'João']
+
+for(var i = 0; i < aluno.length; i++){
+    console.log(aluno[i])
+}
+*/
+
+/*
+SEGUNDA MANEIRA DE CRIAR ARRAY
+var aluno = ['Igor', 'José', 'Marcos', 'Mariana','Joana']
+console.log(aluno.length)
+
+*/
+
+
+
+/*
+PRIMEIRA MANEIRA DE CRIAR ARRAY
+
+var aluno = new Array("Igor", "Jose", "Marcos", "Marina")
+
+console.log(aluno[4])
+
+*/
