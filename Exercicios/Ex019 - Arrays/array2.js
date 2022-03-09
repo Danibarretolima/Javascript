d926a77d6125f3b36364893b34317dff64758d64
Dani Barreto <94726541+Danibarretolima@users.noreply.github.com>
2022-03-09 01:38:44 -0300

    create arrey2.js

---
+++ Exercicios/Ex019 - Arrays/array2.js
@@ -0,0 +1,29 @@
+/*
+PRIMEIRA FORMA
+let aluno = new Array('Maria', 'João', 'Lucas', 'Vini')
+console.log(aluno[3])
+*/
+
+/*
+SEGUNDA FORMA COM A EXPLICAÇÃO DA PROPRIEDADE LENGTH
+let aluno = ['Maria', ' João', 'Lucas', 'Vini']
+console.log(aluno.length) 
+//A propriedade length tem como responsabilidade retornar a quantidade de caracteres de uma string ou o tamanho de um array. Caso a string ou o array esteja vazio, é retornado o valor 0.
+*/
+
+/*
+TERCEIRA MANEIRA USANDO O FOR 
+let aluno = ['Maria', 'João', 'Lucas', 'Vini']
+
+for(let i = 0; i < aluno.length;i++){
+    console.log(aluno[i])
+}
+*/
+
+
+let aluno = ['Maria', 'João', 'Lucas','Vini']
+
+for(let i in aluno){
+    console.log(aluno[i])
+}
+
