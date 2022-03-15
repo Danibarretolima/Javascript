c39f9e63bcd0285bacfcb813d49257489140f5c5
Dani Barreto <94726541+Danibarretolima@users.noreply.github.com>
2022-03-15 17:50:15 -0300

    script.js

---
+++ Exercicios/Ex021 - Juntando tudo/script.js
@@ -0,0 +1,10 @@
+//nome do aluno = nota 1 - nota 2 - mdia - aprovado e reprovado
+
+
+var nomes = ['Igor', 'José', 'Maria']
+var notasa = [7.0, 6.5, 9.5]
+var notasb = [8.0, 7.0, 8.5]
+
+function media(notasa, notasb){
+
+}
